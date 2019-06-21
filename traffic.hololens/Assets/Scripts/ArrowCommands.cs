086a4059bc50225170c18da415c272e8f328803b
Harald Schaffernak <haraldschaffernak@yahoo.de>
2019-06-21 14:54:55 +0200

    Initial version.

---
+++ traffic.hololens/Assets/Scripts/ArrowCommands.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowCommands : MonoBehaviour
+{
+
+    void OnLookAt()
+    {
+        var meshRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+        meshRenderer.enabled = false;
+        //this.gameObject.SetActive(false);
+    }
+}
